7c95884492ad0b706617057064999c77c76707f5
tomas Serpenskas <serpenskas.tomas@gmail.com>
2024-08-02 14:43:01 +0000

    Add topic page and create topic and description area

---
+++ assets/js/script.js
@@ -0,0 +1,129 @@
+type="application/javascript"
+
+function show() {
+
+    let showhide4 = document.getElementById("description4");
+    let showhide3 = document.getElementById("description3");
+    let showhide2 = document.getElementById("description2");
+    let showhide = document.getElementById("description");
+
+    if (showhide.style.display === "none") {
+        showhide.style.display = "flex";
+    } else {
+        showhide.style.display = "none";
+    }
+
+    if (showhide2.style.display === "none") {
+        showhide2.style.display = "none";
+    } else {
+        showhide2.style.display = "none";
+    }
+
+    if (showhide3.style.display === "none") {
+        showhide3.style.display = "none";
+    } else {
+        showhide3.style.display = "none";
+    }
+
+    if (showhide4.style.display === "none") {
+        showhide4.style.display = "none";
+    } else {
+        showhide4.style.display = "none";
+    }
+}
+
+function show2() {
+
+    let showhide4 = document.getElementById("description4");
+    let showhide3 = document.getElementById("description3");
+    let showhide2 = document.getElementById("description2");
+    let showhide = document.getElementById("description");
+
+    if (showhide.style.display === "none") {
+        showhide.style.display = "none";
+    } else {
+        showhide.style.display = "none";
+    }
+
+    if (showhide2.style.display === "none") {
+        showhide2.style.display = "flex";
+    } else {
+        showhide2.style.display = "none";
+    }
+
+    if (showhide3.style.display === "none") {
+        showhide3.style.display = "none";
+    } else {
+        showhide3.style.display = "none";
+    }
+
+    if (showhide4.style.display === "none") {
+        showhide4.style.display = "none";
+    } else {
+        showhide4.style.display = "none";
+    }
+}
+
+function show3() {
+
+    let showhide4 = document.getElementById("description4");
+    let showhide3 = document.getElementById("description3");
+    let showhide2 = document.getElementById("description2");
+    let showhide = document.getElementById("description");
+
+    if (showhide.style.display === "none") {
+        showhide.style.display = "none";
+    } else {
+        showhide.style.display = "none";
+    }
+
+    if (showhide2.style.display === "none") {
+        showhide2.style.display = "none";
+    } else {
+        showhide2.style.display = "none";
+    }
+
+    if (showhide3.style.display === "none") {
+        showhide3.style.display = "flex";
+    } else {
+        showhide3.style.display = "none";
+    }
+
+    if (showhide4.style.display === "none") {
+        showhide4.style.display = "none";
+    } else {
+        showhide4.style.display = "none";
+    }
+}
+
+function show4() {
+
+    let showhide4 = document.getElementById("description4");
+    let showhide3 = document.getElementById("description3");
+    let showhide2 = document.getElementById("description2");
+    let showhide = document.getElementById("description");
+
+    if (showhide.style.display === "none") {
+        showhide.style.display = "none";
+    } else {
+        showhide.style.display = "none";
+    }
+
+    if (showhide2.style.display === "none") {
+        showhide2.style.display = "none";
+    } else {
+        showhide2.style.display = "none";
+    }
+
+    if (showhide3.style.display === "none") {
+        showhide3.style.display = "none";
+    } else {
+        showhide3.style.display = "none";
+    }
+
+    if (showhide4.style.display === "none") {
+        showhide4.style.display = "flex";
+    } else {
+        showhide4.style.display = "none";
+    }
+}
