6db90f767594802d379954f04d2f50b04b527242
tomas Serpenskas <serpenskas.tomas@gmail.com>
2024-08-07 13:12:38 +0000

    Rework showhide function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-type = "application/javascript"
+
 
 // Page Redirects
 function home() {
@@ -6,10 +6,9 @@ function home() {
 }
 
 function topicStart() {
+    
     let username = document.getElementById("username").value;
-
     sessionStorage.setItem("username", username);
-
     window.location.href = "/topic.html";
 }
 
@@ -34,131 +33,77 @@ function upcomingStart() {
 }
 
 // Topic Selection (Show/Hide)
-function show() {
 
-    let showhide4 = document.getElementById("description4");
-    let showhide3 = document.getElementById("description3");
-    let showhide2 = document.getElementById("description2");
-    let showhide = document.getElementById("description");
+let showhide4 = document.getElementById("description4");
+let showhide3 = document.getElementById("description3");
+let showhide2 = document.getElementById("description2");
+let showhide = document.getElementById("description");
 
-    if (showhide.style.display === "none") {
-        showhide.style.display = "flex";
-    } else {
-        showhide.style.display = "none";
-    }
+function show4() {
 
-    if (showhide2.style.display === "none") {
-        showhide2.style.display = "none";
-    } else {
-        showhide2.style.display = "none";
-    }
-
-    if (showhide3.style.display === "none") {
-        showhide3.style.display = "none";
-    } else {
-        showhide3.style.display = "none";
-    }
+    showhide4.style.display = "none";
+    showhide3.style.display = "none";
+    showhide2.style.display = "none";
+    showhide.style.display = "none";
 
     if (showhide4.style.display === "none") {
-        showhide4.style.display = "none";
-    } else {
-        showhide4.style.display = "none";
-    }
-}
-
-function show2() {
-
-    let showhide4 = document.getElementById("description4");
-    let showhide3 = document.getElementById("description3");
-    let showhide2 = document.getElementById("description2");
-    let showhide = document.getElementById("description");
-
-    if (showhide.style.display === "none") {
-        showhide.style.display = "none";
-    } else {
-        showhide.style.display = "none";
-    }
-
-    if (showhide2.style.display === "none") {
-        showhide2.style.display = "flex";
-    } else {
+        showhide4.style.display = "flex";
+        showhide3.style.display = "none";
         showhide2.style.display = "none";
-    }
-
-    if (showhide3.style.display === "none") {
-        showhide3.style.display = "none";
+        showhide.style.display = "none";
     } else {
-        showhide3.style.display = "none";
-    }
-
-    if (showhide4.style.display === "none") {
-        showhide4.style.display = "none";
-    } else {
-        showhide4.style.display = "none";
+        showhide4.style.display === "none";
     }
 }
 
 function show3() {
 
-    let showhide4 = document.getElementById("description4");
-    let showhide3 = document.getElementById("description3");
-    let showhide2 = document.getElementById("description2");
-    let showhide = document.getElementById("description");
-
-    if (showhide.style.display === "none") {
-        showhide.style.display = "none";
-    } else {
-        showhide.style.display = "none";
-    }
-
-    if (showhide2.style.display === "none") {
-        showhide2.style.display = "none";
-    } else {
-        showhide2.style.display = "none";
-    }
+    showhide4.style.display = "none";
+    showhide3.style.display = "none";
+    showhide2.style.display = "none";
+    showhide.style.display = "none";
 
     if (showhide3.style.display === "none") {
+        showhide4.style.display = "none";
         showhide3.style.display = "flex";
+        showhide2.style.display = "none";
+        showhide.style.display = "none";
     } else {
         showhide3.style.display = "none";
-    }
-
-    if (showhide4.style.display === "none") {
-        showhide4.style.display = "none";
-    } else {
-        showhide4.style.display = "none";
     }
 }
 
-function show4() {
+function show2() {
 
-    let showhide4 = document.getElementById("description4");
-    let showhide3 = document.getElementById("description3");
-    let showhide2 = document.getElementById("description2");
-    let showhide = document.getElementById("description");
-
-    if (showhide.style.display === "none") {
-        showhide.style.display = "none";
-    } else {
-        showhide.style.display = "none";
-    }
+    showhide4.style.display = "none";
+    showhide3.style.display = "none";
+    showhide2.style.display = "none";
+    showhide.style.display = "none";
 
     if (showhide2.style.display === "none") {
-        showhide2.style.display = "none";
-    } else {
-        showhide2.style.display = "none";
-    }
-
-    if (showhide3.style.display === "none") {
-        showhide3.style.display = "none";
-    } else {
-        showhide3.style.display = "none";
-    }
-
-    if (showhide4.style.display === "none") {
-        showhide4.style.display = "flex";
-    } else {
         showhide4.style.display = "none";
+        showhide3.style.display = "none";
+        showhide2.style.display = "flex";
+        showhide.style.display = "none";
+    } else {
+        showhide2.style.display = "none";
+    }
+}
+
+function show() {
+
+    showhide4.style.display = "none";
+    showhide3.style.display = "none";
+    showhide2.style.display = "none";
+    showhide.style.display = "none";
+
+    if (showhide.style.display === "none") {
+        showhide4.style.display = "none";
+        showhide3.style.display = "none";
+        showhide2.style.display = "none";
+        showhide.style.display = "flex";
+    } else {
+        showhide.style.display = "none";
     }
 }
 
@@ -204,6 +149,7 @@ function incrementCorrectScore() {
 
     let oldCorrectScore = parseInt(document.getElementById("correctScore").innerText);
     document.getElementById("correctScore").innerText = oldCorrectScore + 1;
+
 }
 
 // Increment incorrect score
@@ -218,15 +164,14 @@ function incrementWrongScore() {
 // Increment question number
 
 function incrementQuestionNumber() {
+
     let questionNumber = document.getElementById("questionNumber");
     var currentNumber = parseInt(questionNumber.innerHTML);
     questionNumber.innerHTML = currentNumber + 1;
+
 }
 
-
 // Display next question / hide last question
-
-
 
 function displayNextQuestion() {
 
@@ -242,32 +187,25 @@ function displayNextQuestion() {
         showhide1.style.display = "none";
         showhide2.style.display = "flex";
     } else if (showhide2.style.display === "flex") {
-
         showhide3.style.display = "flex";
         showhide2.style.display = "none";
-
     } else if (showhide3.style.display === "flex") {
-
         showhide4.style.display = "flex";
         showhide3.style.display = "none";
-
     } else if (showhide4.style.display === "flex") {
         showhide5.style.display = "flex";
         showhide4.style.display = "none";
-
     } else if (showhide5.style.display === "flex") {
-
         showhide7.style.display = "none";
         showhide6.style.display = "flex";
         showhide5.style.display = "none";
         finalScore();
-
     } else {
         // No action needed if none of the elements are displayed.
     }
 }
 
-// Display the achieved score
+// Display the achieved score and the players username
 
 function finalScore() {
 
